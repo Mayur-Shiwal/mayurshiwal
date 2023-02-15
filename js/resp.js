9579b1a36be42846aaf3229e945b2462687c6a24
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-02-15 19:15:37 +0530

    Update resp.js

--- js/resp.js
+++ js/resp.js
@@ -9,3 +9,20 @@ burger.addEventListener('click', ()=>{
     navList.classList.toggle('v-class-resp')
     navbar.classList.toggle('hNav-resp')
 })
+function sendEmail(){
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "mayurshiwal2000@gmail.com",
+        Password : "AEB8C1B657323069C4B0B9AFA4BA6D590923",
+        To : '18be0268@sipnaengg.ac.in',
+        From : "mayurshiwal2000@gmail.com",
+        Subject : "Mail received from contact-form",
+        Body : "Name: " + name + "<br/> User Email: " + email + "<br/> User Message: " + message
+    }).then(
+    message => alert("Success!")
+    );
+}
